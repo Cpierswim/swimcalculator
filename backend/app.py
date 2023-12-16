90eed5b862f0f67a4e83d392612eabe2c0778247
Chris Pierson <cpierswim@gmail.com>
2023-12-15 19:12:13 -0500

    Updated Settings

--- backend/app.py
+++ backend/app.py
@@ -1,37 +1,60 @@
-from flask import Flask, request
+from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager
+from flask_restful import Api
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
+from database.models import db
+from database.schemas import ma
+from resources.auth import LoginResource, RegisterResource
+from resources.cars import AllCarResource, UserCarResource
 from dotenv import load_dotenv
 from os import environ
 
+# Adds variables from .env file to environment
 load_dotenv()
 
-# Create App instance
-app = Flask(__name__)
+# Creates instances of additional libraries
+bcrypt = Bcrypt()
+jwt= JWTManager()
+cors = CORS()
+migrate = Migrate()
 
-# Add DB URI from .env
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('SQLALCHEMY_DATABASE_URI')
+def create_app():
+    """
+    App factory that creates app instance
+    """
+    # Creates app instance
+    app = Flask(__name__)
 
-# Registering App w/ Services
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
-api = Api(app)
-CORS(app)
-Migrate(app, db)
+    # Loads config properties from .env file
+    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('SQLALCHEMY_DATABASE_URI')
+    app.config['JWT_SECRET_KEY'] = environ.get('JWT_SECRET_KEY')
 
-# Models
+    # Registers all routes with API
+    api = create_routes()
+
+    # Registers Flask app with additional libraries created/imported above
+    db.init_app(app)
+    ma.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+    cors.init_app(app)
+    api.init_app(app)
+    migrate.init_app(app, db)
+
+    return app
 
 
-
-# Schemas
-
-
-
-# Resources
-
-
-
-# Routes
+def create_routes():
+    """
+    Creates Flask Restful instance and registers all Resource routes
+    """
+    api = Api()
+    api.add_resource(RegisterResource, '/api/auth/register')
+    api.add_resource(LoginResource, '/api/auth/login')
+    api.add_resource(AllCarResource, '/api/cars')
+    api.add_resource(UserCarResource, '/api/user_cars')
+    # TODO: Create files for your Resources in resources folder, add them here
+    
+    return api
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// General Imports
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+// Pages Imports
+import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+
+// Component Imports
+import Navbar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+
+// Util Imports
+import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <HomePage />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
